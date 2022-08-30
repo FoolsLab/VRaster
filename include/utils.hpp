dac45c19edf3b93942c2c4a8b1b47263a45c1ab5
kiterai <kiterai@foolslab.net>
2022-08-31 07:56:34 +0900

    error file not found log

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <string>
 #include <filesystem>
 #include <fstream>
 
 inline auto file_get_contents(std::filesystem::path path) {
+    if(!std::filesystem::exists(path)){
+        throw std::runtime_error(std::string("file not found: ") + path.string());
+    }
     size_t fileSz = std::filesystem::file_size(path);
     std::ifstream file{ path, std::ios_base::binary };
     std::vector<std::byte> data(fileSz);
